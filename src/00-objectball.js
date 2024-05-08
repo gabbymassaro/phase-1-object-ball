225704eef47003d8dfb933736ba36064bb9e6eb9
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-08 16:36:42 -0400

    this structure is correct but can possibly be refactored

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -3,70 +3,64 @@ function gameObject() {
     home: {
       teamName: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
-        {
-          "Alan Anderson": {
-            number: 0,
-            shoe: 16,
-            points: 22,
-            rebounds: 12,
-            assists: 12,
-            steals: 3,
-            blocks: 1,
-            slamDunks: 1
-          },
-          
-          "Reggie Evans": {
-            number: 30,
-            shoe: 14,
-            points: 12,
-            rebounds: 12,
-            assists: 12,
-            steals: 12,
-            blocks: 12,
-            slamDunks: 7,
-          },
-          
-          "Brook Lopez": {
-            number: 11,
-            shoe: 17,
-            points: 17,
-            rebounds: 19,
-            assists: 10,
-            steals: 3,
-            blocks: 1,
-            slamDunks: 15
-          },
-
-          "Mason Plumlee": {
-            number: 1,
-            shoe: 19,
-            points: 26,
-            rebounds: 12,
-            assists: 6,
-            steals: 3,
-            blocks: 8,
-            slamDunks: 5
-          },
-
-          "Jason Terry": {
-            number: 31,
-            shoe: 15,
-            points: 19,
-            rebounds: 2,
-            assists: 2,
-            steals: 4,
-            blocks: 11,
-            slamDunks: 1
-          },
-        } // end of players
-      ], // end of players
+      players: {
+        "Alan Anderson": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1
+        },
+        "Reggie Evans": {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slamDunks: 7,
+        },
+        "Brook Lopez": {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 15
+        },
+        "Mason Plumlee": {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slamDunks: 5
+        },
+        "Jason Terry": {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slamDunks: 1
+        },
+      } // end of players
     }, // end of home 
       
     away: {
       teamName: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players: [
+      players: 
         {
           "Jeff Adrien": {
             number: 4,
@@ -78,7 +72,6 @@ function gameObject() {
             blocks: 7,
             slamDunks: 2
           },
-
           "Bismak Biyombo": {
             number: 0,
             shoe: 16,
@@ -89,7 +82,6 @@ function gameObject() {
             blocks: 15,
             slamDunks: 10
           },
-
           "DeSagna Diop": {
             number: 2,
             shoe: 14,
@@ -100,7 +92,6 @@ function gameObject() {
             blocks: 5,
             slamDunks: 5
           },
-
           "Ben Gordon": {
             number: 8,
             shoe: 15,
@@ -111,7 +102,6 @@ function gameObject() {
             blocks: 1,
             slamDunks: 0
           },
-
           "Brendan Haywood": {
             number: 33,
             shoe: 15,
@@ -123,9 +113,8 @@ function gameObject() {
             slamDunks: 12
           }
         }, // end of players
-      ], // end of players
-    }, // end of away
-  } // end of something
+      }, // end of away
+    } // end of something
   return something;
 } // end of function
 
